f2cc302f6d3d4b18666f58f12054b9d489664309
Matheus Martins Susin <matheus.susin@venturus.org.br>
2022-09-22 14:10:27 -0300

    Initial Commit

---
+++ ReactionTimer/ReactionTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactionTimerApp.swift
+//  ReactionTimer
+//
+//  Created by Matheus Martins Susin on 2022-09-22.
+//
+
+import SwiftUI
+
+@main
+struct ReactionTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

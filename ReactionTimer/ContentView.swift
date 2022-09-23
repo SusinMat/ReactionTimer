fe8a6ea90f7548653ee55931160502f924f6de49
Matheus Martins Susin <matheus.susin@venturus.org.br>
2022-09-23 17:27:56 -0300

    Add Start Button

--- ReactionTimer/ContentView.swift
+++ ReactionTimer/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var testIsOngoing: Bool = false
+    var buttonText: String {
+        return testIsOngoing ? "Stop" : "Start"
+    }
+
+    var button: some View {
+        Button {
+            buttonTapped()
+        } label: {
+            Text(buttonText)
+                .font(.system(size: 24.0))
+                .foregroundColor(Color(uiColor: .systemBackground))
+                .frame(minWidth: 0.0, maxWidth: .infinity)
+                .padding(.vertical, 4.0)
+        }
+        .background(Color.accentColor)
+        .cornerRadius(10.0)
+        .padding(.horizontal, 16.0)
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            button
+            Spacer().frame(minWidth: .zero)
+        }
+    }
+
+    func buttonTapped() {
+        if testIsOngoing {
+            testIsOngoing = false
+        } else {
+            testIsOngoing = true
+        }
     }
 }
 

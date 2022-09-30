bc6e7524f838ab839b552d34e1239abc8c6ba2ef
Matheus Martins Susin <matheus.susin@venturus.org.br>
2022-09-30 11:01:31 -0300

    Replace Picker with a pair of Buttons

--- ReactionTimer/ContentView.swift
+++ ReactionTimer/ContentView.swift
@@ -9,46 +9,91 @@ import SwiftUI
 
 struct ContentView: View {
     @State var testIsOngoing: Bool = false
-    @State var numberOfButtons: Double = 1.0
+    @State var numberOfButtons: Int = 1
 
-    var buttonText: String {
-        return testIsOngoing ? "Stop" : "Start"
-    }
-
-    var button: some View {
-        Button {
-            buttonTapped()
-        } label: {
-            Text(buttonText)
-                .font(.system(size: 24.0))
-                .foregroundColor(Color(uiColor: .systemBackground))
-                .frame(minWidth: 0.0, maxWidth: .infinity)
-                .padding(.vertical, 4.0)
-        }
-        .background(Color.accentColor)
-        .cornerRadius(10.0)
-        .padding(.horizontal, 16.0)
-    }
-
-    var slider: some View {
-        Slider(value: $numberOfButtons, in: 1...8)
-            .padding(.horizontal, 4.0)
-    }
+    let topStackHeight = 60.0
 
     var body: some View {
         VStack {
             Spacer().frame(height: 16.0)
-            button
-            slider
+            HStack {
+                minusButton
+                startButton
+                plusButton
+            }
+            .padding(.horizontal, 4.0)
             Spacer().frame(minWidth: .zero)
         }
     }
+}
 
-    func buttonTapped() {
+// MARK: - Start/Stop Button
+extension ContentView {
+    var buttonText: AttributedString {
         if testIsOngoing {
-            testIsOngoing = false
+            return "Stop"
         } else {
-            testIsOngoing = true
+            var morphology = Morphology()
+            morphology.number = numberOfButtons == 1 ? .singular : .plural
+            var string = AttributedString("Start (\(numberOfButtons) button)")
+            string.inflect = .explicit(morphology)
+            return string.inflected()
+        }
+    }
+
+    var startButton: some View {
+        Button {
+            if testIsOngoing {
+                testIsOngoing = false
+            } else {
+                testIsOngoing = true
+            }
+        } label: {
+            Text(buttonText)
+                .font(.system(size: 24.0))
+                .foregroundColor(Color(uiColor: .systemBackground))
+                .frame(maxWidth: .infinity, minHeight: topStackHeight)
+        }
+        .background(Color.accentColor)
+        .cornerRadius(10.0)
+    }
+}
+
+// MARK: - Minus and Plus Buttons
+extension ContentView {
+    private struct IncrementButton: View {
+        let textString: String
+        let action: () -> Void
+
+        init(_ textString: String, _ action: @escaping () -> Void) {
+            self.textString = textString
+            self.action = action
+        }
+
+        var body: some View {
+            Button {
+                action()
+            } label: {
+                Text(textString)
+                    .font(.system(size: 28.0))
+                    .padding(.horizontal, 4.0)
+            }
+        }
+    }
+
+    var minusButton: some View {
+        IncrementButton("-") {
+            if numberOfButtons > 1 {
+                numberOfButtons -= 1
+            }
+        }
+    }
+
+    var plusButton: some View {
+        IncrementButton("+") {
+            if numberOfButtons < 8 {
+                numberOfButtons += 1
+            }
         }
     }
 }
